bd363fa56d1f35553876bfcee3013ede31a4ab95
batoolarifa <batoolarifa73@gmail.com>
2025-02-17 16:32:21 +0500

    Fix the deployment issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom"
  import { ToastContainer } from "react-toastify"
  import "react-toastify/dist/ReactToastify.css"
 function App() {
-
+console.log(import.meta.env.VITE_API_BASE_URL)
   return (
   <>
     <Navbar/>
